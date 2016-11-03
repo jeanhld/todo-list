7fd857e436376a4fc133f6928a1a4c417c8ce37b
Jean Holderbaum <jeanhrs@hotmail.com>
2016-11-03 11:31:03 -0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+require('es6-promise').polyfill()
+
+module.exports = {
+    entry: ['babel-polyfill', path.normalize(__dirname + '/src/js/main')],
+    devtool: 'cheap-module-source-map',
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel',
+                test: /\.js$/,
+                include: [path.resolve(__dirname, 'src', 'js')],
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['es2015']
+                }
+            },
+            {
+                loader: 'style!css',
+                test: /\.css$/,
+                include: [path.resolve(__dirname, 'src', 'css')]
+            }
+        ]
+    }
+};
