2cc1ed8343634356a438ddd85b02a112c30e5aa8
Jean Holderbaum <jeanhrs@hotmail.com>
2016-11-03 14:30:56 -0200

    WEB-102

--- src/js/events.js
+++ src/js/events.js
@@ -2,10 +2,15 @@ import {todos} from './state';
 import {listen} from './lib/events';
 import {addTodo, toggleTodoState} from './actions';
 
+function focusOnInput() {
+    document.getElementById("todoInput").focus();
+}
+
 function insertItem(event) {
-  const todoInput = document.getElementById('todoInput');
-  todos.dispatch(addTodo(todoInput.value));
-  event.stopPropagation();
+    const todoInput = document.getElementById('todoInput');
+    todos.dispatch(addTodo(todoInput.value));
+    event.stopPropagation();
+    focusOnInput();
 }
 
 export function registerEventHandlers() {
