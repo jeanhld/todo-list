5690098b9cb0e0b54f187dd71a3c3d6d14bc9aa7
Jean Holderbaum <jeanhrs@hotmail.com>
2016-11-04 14:54:01 -0200

    WEB-110

--- src/js/view.js
+++ src/js/view.js
@@ -17,42 +17,86 @@ function renderApp(input, todoList) {
 }
 
 function renderAddTodoAtTop(input, todoList) {
-    return `<div id="app">
-        ${input}
-        ${todoList}
-        ${isEnabled('filter') ? renderFilter() : ''}
-    </div>`;
+    return `${renderHeader()}
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-centered">
+                        <div id="app">
+                            ${input}
+                            ${todoList}
+                            ${isEnabled('filter') ? renderFilter() : ''}
+                        </div>
+                    </div>
+                </div>
+            </div>`;
 }
 
 function renderAddTodoAtBottom(input, todoList) {
-    return `<div id="app">
-        ${isEnabled('filter') && isEnabled('filterTop')  ? renderFilter() : ''}
-        ${todoList}
-        ${input}
-        ${isEnabled('filter') && !isEnabled('filterTop') ? renderFilter() : ''}
-    </div>`;
+    return `${renderHeader()}
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 col-centered">
+                        <div id="app">
+                            ${isEnabled('filter') && isEnabled('filterTop')  ? renderFilter() : ''}
+                            ${todoList}
+                            ${input}
+                            ${isEnabled('filter') && !isEnabled('filterTop') ? renderFilter() : ''}
+                        </div>
+                    </div>
+                </div>
+            </div>`;
 }
 
 function renderInput() {
-    return `<div class="todo__input"><input type="text" id="todoInput"><button id="addTodo">Add</button></div>`;
+    return `<div class="todo__input input-group">
+                <input type="text" class="form-control" id="todoInput">
+                    <span class="input-group-btn">
+                        <button class="btn btn-primary" id="addTodo" type="button">Add</button>
+                    </span>
+                </input>
+            </div>`;
 }
 
 function renderTodos(todoItems) {
-    return `<ul class="todo">${todoItems}</ul>`;
+    return `<ul class="todo list-group">
+                ${todoItems}
+            </ul>`;
 }
 
 function renderTodoItem(todo) {
     const todoClass = `todo__item todo__item--${todo.done ? 'done' : 'open'}`;
-    return `<li class="${todoClass}">
-        <input class="js_toggle_todo" type="checkbox" data-id="${todo.id}"${todo.done ? ' checked' : ''}>
-        ${todo.text}
-    </li>`;
+
+    return `<li class="${todoClass} list-group-item list-group-item-${todo.done ? 'success' : 'warning' }">
+                <input class="js_toggle_todo" type="checkbox" data-id="${todo.id}"${todo.done ? ' checked' : ''}>
+                ${todo.text}
+            </li>`;
 }
 
 function renderFilter() {
     return `<div class="filter">
-              <input type="radio" name="filterType" value="all" checked=true> Mostrar todos<br>
-              <input type="radio" name="filterType" value="opened"> Somente abertos<br>
-              <input type="radio" name="filterType" value="closed"> Somente fechados<br>
+                <label class="radio-inline">
+                    <input type="radio" name="filterType" value="all" checked=true> Mostrar todos<br>
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="filterType" value="opened"> Somente abertos<br>
+                </label>
+                <label class="radio-inline">
+                    <input type="radio" name="filterType" value="closed"> Somente fechados<br>
+                </label>
             </div>`;
+}
+
+function renderHeader() {
+    return `<div class="container">
+                <div class="row">
+                    <div class="col-lg-5 col-centered">
+                        <div class="jumbotron">
+                            <h1>Simple Todo List</h1>
+                            <p> Inspired by <b> SoftExpert </b> </p>
+                        </div>
+                        <hr/>
+                    </div>
+                </div>
+            </div>
+            `
 }
